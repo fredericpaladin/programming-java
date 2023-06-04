e6abf15c3be274082d1bbb1a0eefc24655a61be5
Frederic Paladin <frederic.paladin@gmail.com>
2023-06-04 18:47:24 -0400

    Added algorithms and data structures

---
+++ DataStructures/src/lists/SingleLinkedListNode.java
@@ -0,0 +1,11 @@
+package lists;
+
+public class SingleLinkedListNode {
+    private int _value;
+    private SingleLinkedListNode _next;
+
+    public SingleLinkedListNode(int value) {
+        _value = value;
+        _next = null;
+    }
+}
